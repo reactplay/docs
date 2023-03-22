c4ae7107bf295abacfff3e1a3fbc18977706d4a3
Petrosdevri <petrosdevri@protonmail.com>
2023-03-22 12:13:31 +0200

    Enabled automatic dark mode for dark mode users

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -125,6 +125,11 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: true,
+        respectPrefersColorScheme: true,
+      }
     }),
 };
 
